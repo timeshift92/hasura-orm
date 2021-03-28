c4712eaa288ebd8cebd8e59df8e32ca4e7fb68d6
timeshift <akhmedov@infosystems.uz>
2021-03-28 14:51:12 +0500

    feat(fix): change order by logic

--- src/hasura.ts
+++ src/hasura.ts
@@ -80,7 +80,7 @@ export default class Hasura {
   }
 
   orderBy(orderBy: OrderBy | any): Hasura {
-    this.addArg('order_by', { '@cl': orderBy })
+    this.addArg('order_by', orderBy)
     // this.addArg('order_by', { '@cl': stringify(orderBy, false, true) })
     return this
   }
